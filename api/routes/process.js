80f0db89738b070c22ad5e1b62bcbf78cd877973
Pratham <prathamsinghal2003@gmail.com>
2023-03-13 17:23:11 +0530

    :rocket: Added arrivial at same time functionality

--- api/routes/process.js
+++ api/routes/process.js
@@ -7,11 +7,6 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/burst-time", (req, res) => {
-  res.status(200).json({
-    message: "Handling POST requests to /burst-time",
-  });
-});
 router.post("/time-quantum", (req, res) => {
   if (!req.body.burstTime)
     return res.status(400).json({ message: "Burst time is required" });
@@ -19,7 +14,7 @@ router.post("/time-quantum", (req, res) => {
   const n = burstTime.length;
   let sumBt = 0;
   burstTime.map((bt) => {
-    sumBt += bt;
+    sumBt += parseInt(bt);
   });
   const timeQuantum = Math.floor(sumBt / n);
   res.status(200).json({
@@ -27,11 +22,60 @@ router.post("/time-quantum", (req, res) => {
     timeQuantum: timeQuantum,
   });
 });
+router.post("/calculation-arrival-same", (req, res) => {
+  console.log(req.body);
+  if (
+    !(
+      req.body.processes &&
+      req.body.burstTime &&
+      req.body.timeQuantum &&
+      req.body.processes.length === req.body.burstTime.length
+    )
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-router.post("/turn-around-time", (req, res) => {
+  const processes = req.body.processes;
+  const n = processes.length;
+  processes.map((process) => {
+    process;
+  });
+  const burstTime = req.body.burstTime;
+  const timeQuantum = req.body.timeQuantum;
+  let waitingTime = [];
+  let turnAroundTime = [];
+  let chart = [];
+  var total_time = 0;
+  let remainingBurstTime = [...burstTime];
+  while (true) {
+    let done = true;
+    for (let i = 0; i < n; i++) {
+      if (remainingBurstTime[i] > 0) {
+        done = false;
+        if (remainingBurstTime[i] > timeQuantum) {
+          remainingBurstTime[i] -= timeQuantum;
+          total_time += parseInt(timeQuantum);
+        } else {
+          total_time += remainingBurstTime[i];
+          turnAroundTime[i] = total_time;
+          waitingTime[i] = total_time - burstTime[i];
+          remainingBurstTime[i] = 0;
+        }
+        chart.push(processes[i]);
+      }
+    }
+    if (done === true) {
+      break;
+    }
+  }
   res.status(200).json({
-    message: "Handling POST requests to /turn-around-time",
+    message: "success",
+    waitingTime: waitingTime,
+    turnAroundTime: turnAroundTime,
+    chart: chart,
+    total_time: total_time,
   });
 });
 
+router.post("/calculation-arrival-different", (req, res) => {});
 module.exports = router;
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const processes = require("./api/routes/process");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/process", processes);
 
 app.listen(PORT, () => {
